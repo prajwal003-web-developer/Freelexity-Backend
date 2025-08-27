6c0c03558331c0b6c6311fbb1e4679b19e5df2f1
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2025-08-28 05:01:38 +0545

    first commit

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["http://localhost:3000","https://free-lexity.vercel.app/"],
     methods:"*"
 }))
 
 const router = require('./routes/index')
 
 app.use("/api",router)
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
 
 
 
