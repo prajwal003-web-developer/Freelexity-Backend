73f6d409d2bcaa8e1dc740918cb977273048889d
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2025-08-28 05:07:01 +0545

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin:["http://localhost:3000","https://free-lexity.vercel.app/"],
+    origin:["http://localhost:3000","https://free-lexity.vercel.app"],
     methods:"*"
 }))
 
